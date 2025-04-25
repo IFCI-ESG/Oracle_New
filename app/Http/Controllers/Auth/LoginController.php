84091b65650f0a657311ac6eff4251c835cd31b8
AsutoshMaharana14 <94016002+AsutoshMaharana14@users.noreply.github.com>
2025-04-26 02:20:23 +0530

    Revert "Update Code with Bug Fixes of Bank and Branch"

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -63,6 +63,6 @@ class LoginController extends Controller
         Auth::guard('web')->logout();
        $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('admin.login');
+        return redirect('login');
     }
 }
